949a1aebaa37f1321bd9827ec378cf6a6a0e6252
Oliver Powell <1012762@student.ccgs.wa.edu.au>
2021-09-06 22:56:48 +0800

    Created my inital tablses and started creation system

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,17 +24,58 @@ namespace Powell_Oliver_Y12CSC_Project2System
         public MainWindow()
         {
             InitializeComponent();
-
-            // Create a new connection to the database
-            var conn = new SQLiteConnection("Data Source=mondial.db");
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //conect to the db
-            //connection.Open();
-
+            Dictionary<string, List<String>> TablesToCreate = new Dictionary<string, List<String>>()
+            { { "Supplier",new List<String>() { "SupplierID~IPA", "Contact~V064N", "ShippingState~V064" , "Title~V020N" } },
+            { "Order",new List<String>() { "OrderID~IPA", "SupplierID~IF", "Subtotal~CN" , "ArrivalDate~D" } },
+            { "Item",new List<String>() {"ProductID~IF", "OrderID~IF", "Quatnity~IN" , "ShippingCost~CN", "Total~CN" } },
+            { "Product",new List<String>() { "ProductID~IPA", "SeriesID~IF", "Name~TN", "Description~T", "Type~V032N", "Colour~V032N", "Size~V032N", "UnitCost~CN" , "Discount~IN" } },
+            { "Series",new List<String>() { "SeriesID~IPA", "EmployeeID~IF", "Name~TN", "Description~TN", "TotalProducts~V020N", "StartDate~D"} },
+            { "Customer",new List<String>() { "CustomerID~IPA","FirstName~V020N","LastName~V020N", "PostCode~IN" , "ContactInfo~T064N" } },
+            { "Employee",new List<String>() { "EmployeeID~IPA", "FirstName~V020N","LastName~V020N","JobTitle~V020N", "PostCode~IN" , "ContactInfo~T064N" } },
+            { "Social",new List<String>() { "SocialID~IPA","EmployeeID~IF","Name~V040N","Type~V020N"} }, };
+            Dictionary<char, string> DataTypes = new Dictionary<char, string>() 
+            { { 'I', "INTEGER" },{ 'A', "AUTOINCREMENT" },{ 'P', "PRIMARY KEY" },{ 'A', "AUTOINCREMENT" },{ 'T', "TEXT" },{ 'N', "NOT NULL" },{ 'C', "DOUBLE(20,2)" },{ 'D', "DATE" }};
+            /* 
+             * V003 - Var Char(3)
+             * F - foreign key
+             */
+            //Setting up the database tables
+            foreach (var Tb in TablesToCreate.Keys)
+            {
+                string toQuery = "";
+                string AtName;
+                string AtConstraints;
+                toQuery += $"DROP TABLE IF EXISTS {Tb}";
+                foreach (string Attr in TablesToCreate[Tb])
+                {
+                    AtName  = Attr.Split('~')[0];
+                    AtConstraints = Attr.Split('~')[1];
+                    System.Diagnostics.Debug.WriteLine($"{AtName} and {AtConstraints}");
+                }
+            }
         }
+        public static void QueryDatabase(string query)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=fuelwatch.db"))
+            {
+                try
+                {
+                    connection.Open();
+                    SQLiteCommand Query = new SQLiteCommand(query, connection);
+                    Query.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    System.Windows.MessageBox.Show(e.Message);
+                    connection.Close();
+                    return;
+                }
+            }
+        }
+
     }
 }
